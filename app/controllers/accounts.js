35ac86a8745d6598dd5fc8aaf54da3e2d42d4b1a
Shane Fitzpatrick <fitzpatrick.shane@gmail.com>
2021-03-15 03:34:04 +0000

    Added views and partials from donations lab. Added simple user persistance and "donation" persistance. Next to do is to refactor all views and partials to POI interface

---
+++ app/controllers/accounts.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const Accounts = {
+  index: {
+    handler: function (request, h) {
+      return h.view("main", { title: "Welcome to Donations" });
+    },
+  },
+  showSignup: {
+    handler: function (request, h) {
+      return h.view("signup", { title: "Sign up for Donations" });
+    },
+  },
+  signup: {
+    handler: function (request, h) {
+      const user = request.payload;
+      this.users[user.email] = user;
+      this.currentUser = user;
+      return h.redirect("/home");
+    },
+  },
+  showLogin: {
+    handler: function (request, h) {
+      return h.view("login", { title: "Login to Donations" });
+    },
+  },
+  login: {
+    handler: function (request, h) {
+      const user = request.payload;
+      if (user.email in this.users && user.password === this.users[user.email].password) {
+        this.currentUser = this.users[user.email];
+        return h.redirect("/home");
+      }
+      return h.redirect("/");
+    },
+  },
+  logout: {
+    handler: function (request, h) {
+      return h.redirect("/");
+    },
+  },
+};
+
+module.exports = Accounts;
